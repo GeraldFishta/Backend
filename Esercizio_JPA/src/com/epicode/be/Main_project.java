74c1f55ee85de4da78e9cbd4ea27f460bc6d3ce6
GeraldFishta <ge.fishta@gmail.com>
2023-07-12 17:30:45 +0200

    es,finito

--- Esercizio_JPA/src/com/epicode/be/Main_project.java
+++ Esercizio_JPA/src/com/epicode/be/Main_project.java
@@ -1,23 +1,37 @@
 package com.epicode.be;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import utils.JpaUtil;
 
 public class Main_project {
-	
-	static EntityManagerFactory  emf = Persistence.createEntityManagerFactory("paramsDb");
-	static EntityManager em = emf.createEntityManager();
 
-	public static void main(String[] args) {
-		
-		
-		
-	}
+    public static void main(String[] args) {
+        Evento evento = new Evento();
+        evento.setTitolo("Evento di prova");
+        evento.setDataEvento(new Date());
+        evento.setDescrizione("Descrizione dell'evento");
+        evento.setTipoEvento(TipoEvento.PUBBLICO);
+        evento.setNumeroMassimoPartecipanti(100);
 
+        EventoDAO eventoDAO = new EventoDAO();
+
+        eventoDAO.save(evento);
+        System.out.println("Evento salvato con successo. ID: " + evento.getId());
+
+        Long eventoId = evento.getId();
+        Evento eventoRecuperato = eventoDAO.getById(eventoId);
+        System.out.println("Evento recuperato: " + eventoRecuperato.getTitolo());
+
+        eventoRecuperato.setDescrizione("Nuova descrizione dell'evento");
+        eventoDAO.refresh(eventoRecuperato);
+        System.out.println("Evento aggiornato con successo.");
+
+
+        eventoDAO.delete(eventoRecuperato);
+        System.out.println("Evento eliminato.");
+
+      
+        JpaUtil.getEntityManagerFactory().close();
+    }
 }
