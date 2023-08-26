0bc52a6fd639f17257a62d73509bb8b6b3f636fa
GeraldFishta <ge.fishta@gmail.com>
2023-08-26 18:38:44 +0200

    esercizio incendio test

--- GestioneIncendi/ControllerIncendi/src/main/java/com/epicode/Spring/ControllerIncendiApplication.java
+++ GestioneIncendi/ControllerIncendi/src/main/java/com/epicode/Spring/ControllerIncendiApplication.java
@@ -2,6 +2,8 @@ package com.epicode.Spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
 public class ControllerIncendiApplication {
--- GestioneIncendi/ControllerIncendi/src/test/java/com/epicode/Spring/ControllerIncendiApplicationTests.java
+++ GestioneIncendi/ControllerIncendi/src/test/java/com/epicode/Spring/ControllerIncendiApplicationTests.java
@@ -1,13 +1,42 @@
 package com.epicode.Spring;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ControllerIncendiApplicationTests {
 
+	@Autowired
+    private MockMvc mockMvc;
+	
 	@Test
-	void contextLoads() {
-	}
+	 void testControlSonde() throws Exception {
+        mockMvc.perform(get("/alarm")
+                .param("idsonda", "1")
+                .param("lat", "25.12345")
+                .param("lon", "45.67890")
+                .param("smokelevel", "6"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Allarme!"));
+    }
 
+	@Test
+	void testControlSondeBasse() throws Exception {
+		mockMvc.perform(get("/alarm")
+				.param("idsonda", "1")
+				.param("lat", "25.12345")
+				.param("lon", "45.67890")
+				.param("smokelevel", "3"))
+		.andExpect(status().isOk())
+		.andExpect(content().string("Allarme!"));
+	}
+	
 }
